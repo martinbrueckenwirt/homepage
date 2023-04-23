ca3c2284f0857e0e723081ec4bd3e1c885f4e63a
martinbrueckenwirt <90929503+martinbrueckenwirt@users.noreply.github.com>
2023-04-23 08:12:53 +0200

    reduce to min

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,12 @@
-'use client'
 
-import Link from 'next/link'
-import useTranslation from 'next-translate/useTranslation'
-import ClientCode from '../components/client-code'
-import Header from '../components/header'
-import LanguageSwitcher from '../components/languageSwitcher'
-import Gallery from '../components/gallery.jsx'
-import GalleryA from '../components/gallery_a.jsx'
 
-import Counter from '../components/counter'
-import imageList from "../../data/image";
 import styles from "../components/gallery.module.css"
 import Image from "next/image"
 
 export default function Page() {
-   const { t, lang } = useTranslation('common')
-  const title = t('title')
-  const menueAppartement = t('menue')
+  
 
-  let gridImages =[]; /*Liste der anzuzeigenden Bilder */
-  gridImages = imageList[0].images;
-  let image = gridImages[0];
-  /* 
-      <Gallery id="typeA" /> */  
-     
- /* <Header />
-
-      <LanguageSwitcher />
-      <h1>{title}</h1>
-        
-      <div>
-        <Link href={`/second-page?lang=${lang}`} as={`/${lang}/second-page`}>
-          ➡️
-        </Link>
-      </div>
-
-      <div>
-        <Link href={`/apartments?lang=${lang}`} as={`/${lang}/apartments`}>
-          {menueAppartement}
-         </Link>
-      </div>
-
-      <div>
-        <Link href={`/apartments/gartenblick?lang=${lang}`} as={`/${lang}/apartments/gartenblick`}>
-          Gartenblick
-         </Link>
-      </div>
-      <p></p>
-      <ClientCode />
-
-      <p></p>
-
-       src= {"/P2535132-1.webp"}
-*/
-
+  
   return (
     <> 
       

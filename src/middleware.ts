4eff4a4e41112f36534fa2b171bc401805e39069
martinbrueckenwirt <90929503+martinbrueckenwirt@users.noreply.github.com>
2023-04-30 14:44:25 +0200

    middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,3 +10,15 @@ export function middleware(request: NextRequest) {
   return NextResponse.rewrite(request.nextUrl)
 
 }
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}

defbaf369b30ac319234c5054280f68d29f7e7fe
martinbrueckenwirt <90929503+martinbrueckenwirt@users.noreply.github.com>
2023-12-23 09:00:15 +0100

    global.css

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -36,10 +36,10 @@ export default function Home() {
     
 return (
     <>
-   <section className = {heroSection}>
-   <h1 className ={`${heroFont.className} ${globalStyles.heroText}`} >{t('title')}</h1>
+   <section className = "heroSection">
+   <h1 className ={`${heroFont.className} heroText`} >{t('title')}</h1>
    
-    <div className = {styles.content}>
+    <div className = {`${heroFont.className} content`}>
         <p>
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.   
 

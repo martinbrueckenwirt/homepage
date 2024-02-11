1c2a2a9319cb8d9d4df5154828bc10a26ca2f7ec
martinbrueckenwirt <90929503+martinbrueckenwirt@users.noreply.github.com>
2024-02-11 19:02:24 +0100

    Update booking.tsx

--- src/app/[locale]/components/booking.tsx
+++ src/app/[locale]/components/booking.tsx
@@ -1,71 +1,28 @@
-"use client";
-import React, { use } from 'react'
-import styles from './booking.module.css';
-import { Booking } from './types'
-import {useLocale} from 'next-intl';
-import {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
+export default function Booking(props) {
+	// ...existing code...
 
-
-export default function Booking(props: Booking) {
-	let myId=props.id;
-	const [myLanguage, setMyLanguage] = useState<string>('');
-
-	const [htmlString, setHtmlString] = useState<string>(`
-	<div id="vri-container-${myId}"></div>
-	<script type="text/javascript">
-		( function ( v, i, o, m, a ){
-			if ( !( o in v ) ) {
-				v.vioma_vri=o;
-				v[o] || ( v[o] = function ( ){ ( v[o].q = v[o].q || [] ).push ( arguments ); } );
-				m = i.createElement( 'script' ), a = i.scripts[0];
-				m.src = 'https://cst-client-hotel-brueckenwirt.viomassl.com/js/vri/vri.js';
-				a.parentNode.insertBefore ( m, a );
-			}
-		} )( window, document, 'vcst' );
-		vcst( {load: 'init', url: 'https://cst-client-hotel-brueckenwirt.viomassl.com/', set_language: 'de'} );
-		vcst( {id: ${JSON.stringify(myId)}} );
-	</script>
-`);
-
-const [htmlStringEN, setHtmlStringEN] = useState<string>(`
-<div id="vri-container-20845"></div>
-<script type="text/javascript">
-	( function ( v, i, o, m, a ){
-		if ( !( o in v ) ) {
-			v.vioma_vri=o;
-			v[o] || ( v[o] = function ( ){ ( v[o].q = v[o].q || [] ).push ( arguments ); } );
-			m = i.createElement( 'script' ), a = i.scripts[0];
-			m.src = 'https://cst-client-hotel-brueckenwirt.viomassl.com/js/vri/vri.js';
-			a.parentNode.insertBefore ( m, a );
-		}
-	} )( window, document, 'vcst' );
-	vcst( {load: 'init', url: 'https://cst-client-hotel-brueckenwirt.viomassl.com/', set_language: 'en'} );
-	vcst( {id: 20845} );
-</script>
-`);
- 
-
-	const locale = useLocale();
-	
+	const [isLoaded, setIsLoaded] = useState(false);
 
 	useEffect(() => {
-		setMyLanguage('de'); /*default*/
-		console.log('booking useEffect ', locale);
-	},[]);
+		const timer = setTimeout(() => {
+			setIsLoaded(true);
+		}, 2000); // Adjust the delay time as needed
 
+		return () => clearTimeout(timer);
+	}, []);
 
-useEffect(() => {
-	setMyLanguage(locale);
-    console.log('booking useEffect ', locale);
-},[locale]);
- 
+	// ...existing code...
 
-return (
-	<>
-	HELLO WORLD
-	<div dangerouslySetInnerHTML={{ __html: myLanguage === 'de' ? 'DE' : myLanguage === 'en' ? 'EN' : '' }}/> 	
-	{myLanguage === 'de' && <div dangerouslySetInnerHTML={{ __html: htmlString }}/>}
+	return (
+		<>
+			HELLO WORLD
+			<div dangerouslySetInnerHTML={{ __html: myLanguage === 'de' ? 'DE' : myLanguage === 'en' ? 'EN' : '' }}/>
+			{myLanguage === 'de' && isLoaded && <div dangerouslySetInnerHTML={{ __html: htmlString }}/>}
+		</>
+	);
+}
 	{myLanguage === 'en' && <div dangerouslySetInnerHTML={{ __html: htmlStringEN }}/>}
 	</>
 )

801c34ff41dc674b8d330bd98e8a486458ebff58
martinbrueckenwirt <90929503+martinbrueckenwirt@users.noreply.github.com>
2024-02-27 22:12:15 +0100

    main page newdesign

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,18 +4,17 @@ import React from 'react';
 import { useLocale, useTranslations } from 'next-intl';
 import { Metadata } from 'next';
 import { heroFont, h1Font, h2Font, h3Font, textFont, footerFont } from '@/src/functions/fonts';
-import  Hero from './components/heroModule';
-/*import HeroImage from '../../../public/P1540356-25-1.webp';*/
+import Hero from './components/heroModule';
+import Link from 'next/link';
 import HeroImage from '../../../public/P1540356-2550x1274-180-60.webp';
-/*import HeroImage from '../../../public/P1190113.webp';*/
-import TextModule from './components/textModule';
+import Image from 'next/image';
+import Appartments from '../../../public/P1360618-800x630-180-60.webp';
+import Cafe from '../../../public/P1180423.jpg';
+import Aktivitaeten from '../../../public/P2566866-1.jpg';
+import History from '../../../public/BrueckenwirtVorHochwasser.webp';
+import Background from '../../../public/Landkarte_50proz.png';
 
-import ApartmentModule from './components/apartementModule';
-import CafeModule from './components/cafeModule';
-import History from './components/history';
 import Booking from './components/booking';
-import Script from 'next/script';
-// import viomaIntegration from '../../../public/scripts/viomaIntegration.js';
 
 
 import {getTranslations} from 'next-intl/server';
@@ -38,10 +37,6 @@ export async function generateMetadata({params: {locale}}: {params: {locale: str
   };
 }
 
-
-
-
-
 export default function Home({params: {locale}}: {params: {locale: any}}) {
     
     /*const locale = useLocale();*/
@@ -50,26 +45,24 @@ export default function Home({params: {locale}}: {params: {locale: any}}) {
 
     let imageAlt: string="";
     imageAlt = t('heroimagealt');
-
-    let text1: string ="";
-    let cafeIntro:string=""
-    let appartmentContainerHeadline: string ="";
-    appartmentContainerHeadline = t("appartmentContainerHeadline"); 
-    let cafeContainerHeadline: string ="";
-    cafeContainerHeadline = t("cafeContainerHeadline"); 
-    let textCafe: string ="";
-    let history: string ="";
-    history = t("history"); 
-    let historyContainerHeadline: string ="";
-    historyContainerHeadline = t("historyContainerHeadline"); 
-
-   
-   
-
+    let altAppartment: string = t('altAppartment');
+    let apparmentHeadline: string = t('appartmentContainerHeadline');
+    let appartmentText: string = t('appartmentText');
+    let cafeHeadline: string = t('cafeHeadline');
+    let cafeText: string = t('cafeText');
+    let altCafe: string = t('altCafe');
+    let aktivitaetenHeadline: string = t('aktivitaetenHeadline');
+    let aktivitaetenText: string = t('aktivitaetenText');
+    let altAktivitaeten: string = t('altAktivitaeten');
+    let altHistory: string = t('altHistory');
+    let historyText: string = t('historyText');
+    let historyText2: string = t('historyText2');
+    let historyText3: string = t('historyText3');
+    
+    
     /* Muster für zb "<important> abc </important" */
     /*text1 = t.markup('text1',{
         important:(chunks) => `<b>${chunks}</b>`
-   
     })    */ 
    
     
@@ -84,61 +77,145 @@ return (
                 imageUrlAlt = {imageAlt}
                 calledFrom = "home">
             </Hero>
-    <section className={styles.section}>
-                <h1 className={`${heroFont.className} ${styles.h1}`}>    
-                    <div dangerouslySetInnerHTML={{__html: t.raw('headline')}} />
-                    </h1>
-            <div className={styles.textContainer}>
-                <div className={`${h1Font.className} ${styles.text}`}>
-                    <div dangerouslySetInnerHTML={{__html: t.raw('text1')}} />
-                 </div>
+    <section className={styles.spacerHead}>
+        <h1 className={`${heroFont.className} ${styles.h1}`}>    
+            <div dangerouslySetInnerHTML={{__html: t.raw('headline')}} />
+            </h1>
+        <div className={styles.textTeaserContainer}>
+            <div className={`${h1Font.className} ${styles.teaser}`}>
+                <div dangerouslySetInnerHTML={{__html: t.raw('welcome')}} />
             </div>
+        </div>
 
      <Booking apartmentId={"20845"}/>
-
-
-    <h2 className={`${h1Font.className} ${styles.appartmentContainer}`}>
-        {appartmentContainerHeadline}
-     </h2>
-     <div className={styles.modulesContainer}>
-        <ApartmentModule appType = {'A'} /> 
-        <ApartmentModule appType={'C'} />       
-        <ApartmentModule appType = {'B'} />
-       
-        <ApartmentModule appType = {'D'} /> 
-        <ApartmentModule appType = {'E'} />  
-       <TextModule /> 
-
-        </div>
     </section>
-
-    <section>
-
-    <div className={styles.textContainer}>
-                <div className={`${h1Font.className} ${styles.text}`}>
-                    <div dangerouslySetInnerHTML={{__html: t.raw('cafeIntro')}} />
+         
+        <div className={styles.outerWrapper}>
+            <div className={styles.innerWrapper} >
+                       
+                <div className={styles.box1}>
+                    <Image
+                        className={styles.image}
+                        src =  {Appartments}
+                        width = {800}
+                        height={600}
                     
+                        alt={altAppartment}
+                        priority = {false}
+                        // sizes ="(max-width:580px) 100vw, (max-width:1024px) 50vw, 33vw)"
+                        // layout="responsive"
+                    />
+                </div>  
+                <div className={styles.box2} >
+                    <div className={styles.textContainer}>
+                        <div className={`${h1Font.className} ${styles.h2}`} >    
+                            {apparmentHeadline}
+                        </div>
+                        <div className={`${h1Font.className} ${styles.text}`} >
+                            {appartmentText}
+                        </div>
+                        <Link href="#">
+                            <div className={styles.button}>
+                                <button className={styles.button}> {t('details')}</button>        
+                             </div>
+                        </Link>
+                       
+                    </div>
                 </div>
-    </div>
-    <h2 className={`${h1Font.className} ${styles.appartmentContainer}`}>
-        {cafeContainerHeadline}
-    </h2>
-<CafeModule /> 
-    </section>
 
-    <section className='section'>
+                <div className={styles.box3} >    
+                    <div className={styles.textContainer} >
+                        <div className={`${h1Font.className} ${styles.h2}`} >   
+                                {cafeHeadline}        
+                        </div>
+                        <div className={styles.text} >
+                            {cafeText}
+                        </div>
+                            <Link href="#">
+                                <div className={styles.button}>
+                                    <button className={styles.button}> {t('details')}</button>        
+                                </div>
+                            </Link>
+                    </div>
+                </div>
+                        
+                <div className={styles.box4}> 
+                        <div className={styles.image} >
+                            <Image
+                                className={styles.image}
+                                src =  {Cafe}
+                                width = {800}
+                                height={600}
+                            
+                                alt={altCafe}
+                                priority = {false}
+                                // sizes ="(max-width:580px) 100vw, (max-width:1024px) 50vw, 33vw)"
+                                // layout="responsive"
+                            />
+                        </div>
+                </div>
+               
+                <div className={styles.box5}> 
+                        <div className={styles.image} >
+                            <Image
+                                className={styles.image}
+                                src =  {Aktivitaeten}
+                                width = {800}
+                                height={600}
+                            
+                                alt={altAktivitaeten}
+                                priority = {false}
+                                // sizes ="(max-width:580px) 100vw, (max-width:1024px) 50vw, 33vw)"
+                                // layout="responsive"
+                            />
+                        </div>
+                </div>   
 
-<div className={styles.textContainer}>
-            <div className={`${h1Font.className} ${styles.text}`}>
-                <div dangerouslySetInnerHTML={{__html: t.raw('history')}} />
-                
+                <div className={styles.box6} >    
+                    <div className={styles.textContainer} >
+                        <div className={`${h1Font.className} ${styles.h2}`} >   
+                                {aktivitaetenHeadline}        
+                        </div>
+                        <div className={styles.text} >
+                            {aktivitaetenText}
+                        </div>
+                            <Link href="#">
+                                <div className={styles.button}>
+                                    <button className={styles.button}> {t('details')}</button>        
+                                </div>
+                            </Link>
+                    </div>
+                </div>
             </div>
         </div>
-<h2 className={`${h1Font.className} ${styles.appartmentContainer}`}>
-    {historyContainerHeadline}
- </h2>
-<History /> 
-</section>
+    <section>
+        <div className={styles.historyContainer}>            
+                
+                <div className={styles.historyImage}>
+                    <Image
+                        className={styles.image}
+                        src =  {History}
+                        width = {400}
+                        height={300}
+                    
+                        alt={altHistory}
+                        priority = {false}
+                        // sizes ="(max-width:580px) 100vw, (max-width:1024px) 50vw, 33vw)"
+                        // layout="responsive"
+                    />            
+                </div>
+                
+                <div className={styles.historyText}>
+                            {historyText}
+                            <br></br>
+                            {historyText2}
+                            <br></br>
+                            {historyText3}
+                </div>
+                    
+            
+        </div>        
+    </section>    
 
 </div >
 
